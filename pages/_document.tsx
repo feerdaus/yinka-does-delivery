9b886de2fbd65f49932a9b632a97a48ac55f05d7
feerdaus <firdauslawal@gmail.com>
2023-01-17 15:03:43 +0100

    Initiate create-next-app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* PWA primary color */}
+          <meta key="theme-color" name="theme-color" content="#FFFAF3" />
+          <meta name="robots" content="noindex" />
+          <link key="favicon" rel="icon" href="/favicon.jpg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+// https://fonts.google.com/share?selection.family=Lobster%7CNoto%20Sans:wght@400;500;600;700
